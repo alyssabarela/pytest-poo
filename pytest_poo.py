16f40c88dd54d5c127ec6c6166daf2e296726d05
Alyssa <alyssa.barela17@gmail.com>
2015-02-12 10:26:19 -0700

    making my poo cooler

--- pytest_poo.py
+++ pytest_poo.py
@@ -11,15 +11,5 @@ def pytest_addoption(parser):
 
 
 def pytest_report_teststatus(report):
-    if (not pytest.config.option.poo) or ('poo' not in report.keywords) or (report.when != 'call'):
-        return
-
-    if (pytest.config.option.verbose == -1 and report.passed) or pytest.config.option.verbose >= 0:
+    if pytest.config.option.poo and report.failed:
         return (report.outcome, PILE_OF_POO, '%s (%s)' % (report.outcome.upper(), PILE_OF_POO))
-
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        'markers',
-        'poo: Mark the test as crappy. When using --poo, pile of poo '
-        'will be shown with the test outcome.')
